a2927a9263cd1228683be3f6011423b997bbcc33
unknown <abenshyngys@gmail.com>
2018-01-16 13:22:47 -0500

    First add

---
+++ 1pp/1pp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1pp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] s = line.Split(' ');
+            int sum = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int cnt = 0;
+                sum = int.Parse(s[i]);
+                for (int j = 2; j <= Math.Sqrt(sum); j++)
+                {
+                    if (sum % j == 0)
+                    {
+                        cnt++;
+                        break;
+                    }
+
+                }
+                if (cnt == 0 && sum != 1)
+                {
+                    Console.WriteLine(sum);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
