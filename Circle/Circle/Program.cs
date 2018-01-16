cf07f50e71253fbf559983ec24cdca8fae601ee9
unknown <abenshyngys@gmail.com>
2018-01-16 14:01:00 -0500

    Third Task

---
+++ Circle/Circle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double r = double.Parse(Console.ReadLine());
+
+            Circle c = new Circle(r);
+            //c.findArea();
+
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
